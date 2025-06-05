7459384447b3df1694adda610e75fa710aa5d90d
papayashark <animetianochka@gmail.com>
2025-06-05 20:01:44 +0200

    feat(schema): added user mongo schema

---
+++ model/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+var userSchema = mongoose.Schema({
+
+    name:{
+        type:  String
+    },
+    email: {
+        type:String
+    },
+    age: {
+        type:Number
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+module.exports = mongoose.model("UserInfo", userSchema)
