74e27e913c5f91473f547a7ebda086768d0f48ff
papayashark <animetianochka@gmail.com>
2025-06-09 17:53:12 +0200

    fix(user):added user schema validation

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,31 @@
 
-exports.getUser = (req,res) => {
-    res.send("bonjour")
+const User  = require("../model/User")
+
+exports.postUserInfo = async (req,res) => {
+    try {
+        const {name,email,age} = req.body
+
+        if (!name){
+            return res.status(422).json({error:"Please enter your name"})
+        }
+
+        if (!email){
+            return res.status(422).json({error:"Please enter your email"})
+        }
+
+        if (!age){
+            return res.status(422).json({error:"Please enter your age"})
+        }
+        const userInfo = new User({
+            name,
+            email,
+            age
+        })
+
+        const userPostResult = await User.create(userInfo)
+        res.status(201).json(userPostResult)
+
+    } catch (error) {
+        res.status(400).json({error: "Went wrong"})
+    }
 }
